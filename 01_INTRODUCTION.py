c37f61f847c500283f763bc6ff88c58910deda48
EMILY <emgriggs123@gmail.com>
2023-06-13 13:24:10 +1200

    v1 of the introduction component part 1.1

--- 01_INTRODUCTION.py
+++ 01_INTRODUCTION.py
@@ -1,1 +1,17 @@
-print('hello world')
+error_highlight = '*****'
+
+def num_checker(nameInput):
+    while True:
+        nameStr = input(nameInput)
+        if any(char.isdigit() for char in nameStr):
+            print('{}Please do not add integers{}'.format(error_highlight, error_highlight))
+        elif len(nameStr) == 0:
+            print('{}Please enter a name!{}'.format(error_highlight, error_highlight))
+        else:
+            break
+    return nameStr
+        
+
+print('Hello! Welcome to the cipher program')
+name = num_checker('Please enter your name: ')
+print('Welcome ' + name)
