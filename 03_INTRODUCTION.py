66cd21f35801682369b4a1a154803240ae062579
EMILY <emgriggs123@gmail.com>
2023-06-14 23:17:39 +1200

    1.3 finished different cipher choice inputs :)

--- 03_INTRODUCTION.py
+++ 03_INTRODUCTION.py
@@ -51,6 +51,24 @@ def userEnDc(message):
             # prints error if user does anything else
             print('{}Please enter 1 or 2!{}'.format(error_highlight, error_highlight))
 
+def cipherChoice(choice):
+    while True:
+        # try & except to catch erros
+        try:
+            caplinput = int(input(choice))
+            # sets to true if user picks caeser
+            if caplinput == 1:
+                return True
+                break
+            # sets to false if user picks playfair
+            elif caplinput == 2:
+                return False
+                break
+        except ValueError:
+            # prints error if user does anything else
+            print('{}Please enter 1 or 2!{}'.format(error_highlight, error_highlight))
+
+
 # Main
 
 # gets user's name and welcomes
@@ -63,10 +81,21 @@ instructions('Would you like to read the instructions, (enter "y" or "n"):')
 print('program continues')
 
 # ask if user wants to encrypt or depcrypt
-enDc = userEnDc('Would you like to encode or decode a message? \n [Press 1 for encode or press 2 for decode]')
+enDc = userEnDc('Would you like to encode or decode a message? \n [Press 1 to encode message or press 2 to decode it]:')
 # encodes if value is true 
 if enDc:
-    print('encode')
+    # ask user which cipher
+    enCipher = cipherChoice('Would you like to encode using the Caser cipher or the Playfair cipher? \n [Press 1 for Caeser (sn) or press 2 for Playfair]:')
+    # if true caeser
+    if enCipher:
+        print('caeser(sn) encode')
+    # if false playfair
+    else:
+        print('playfair encode')
 # decodes if value is false
 else:
-    print('decode')
+    dcCipher = cipherChoice('Would you like to decode using the Caser cipher or the Playfair cipher? \n [Press 1 for Caeser (sn) or press 2 for Playfair]:')
+    if dcCipher:
+        print('caeser(sn) decode')
+    else:
+        print('playfair decode')
